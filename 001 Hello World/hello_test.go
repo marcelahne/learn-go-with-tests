0dc605dafeea44f7ab660850cdefe138fdab1d45
Marcel Ahne <marcel.ahne@limango.com>
2026-05-15 09:55:50 +0200

    Add default for empty string input

--- 001 Hello World/hello_test.go
+++ 001 Hello World/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Marcel")
-	want := "Hello, Marcel"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Marcel")
+		want := "Hello, Marcel"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
