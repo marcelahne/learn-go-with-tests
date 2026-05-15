18b493cd16f5001bfa8f2c5a509e43de6d60ea4c
Marcel Ahne <marcel.ahne@limango.com>
2026-05-15 10:28:59 +0200

    Refactored test code

--- 001 Hello World/hello_test.go
+++ 001 Hello World/hello_test.go
@@ -5,19 +5,20 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Marcel")
-		want := "Hello, Marcel"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		want := "Hello, Msdfarcel"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
