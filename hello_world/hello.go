61b8b84f8f64aef636f8301c598b1abf9d4130f4
Marcel Ahne <marcel.ahne@limango.com>
2026-05-22 16:26:34 +0200

    Create a better package for hello_world

---
+++ hello_world/hello.go
@@ -0,0 +1,39 @@
+package hello_world
+
+import "fmt"
+
+const (
+	german  = "German"
+	french  = "French"
+	spanish = "Spanish"
+
+	defaultPrefix = "Hello, "
+	germanPrefix  = "Hallo, "
+	frenchPrefix  = "Bonjour, "
+	spanishPrefix = "Hola, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+	case german:
+		return germanPrefix
+	case french:
+		return frenchPrefix
+	case spanish:
+		return spanishPrefix
+	}
+
+	return defaultPrefix
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
